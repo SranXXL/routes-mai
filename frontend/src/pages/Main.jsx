fef59370e464c5747fa6a65c66edca69e975577a
Aleksandr Fursov <a.fursov@hh.ru>
2020-11-03 20:44:52 +0300

    R-2 add bootstrap

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
+import Button from 'react-bootstrap/Button';
+
 import Header from '../components/Header.jsx';
 
-const Main = () => <Header name="MAN MAPS" />;
+const Main = () => (
+    <>
+        <Header name="MAN MAPS" />
+        <Button variant="primary" onClick={() => {}}>
+            Построить маршрут
+        </Button>
+    </>
+);
 
 export default Main;
