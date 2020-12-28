ca8f484ef38d0c9c335205e946a66b32b3538e0d
Aleksandr Fursov <a.fursov@hh.ru>
2020-12-28 04:03:03 +0300

    R-13 handle empty result

--- backend/handlers/findRoutes.js
+++ backend/handlers/findRoutes.js
@@ -21,17 +21,26 @@ const findRoutes = (client) => async (ctx) => {
     const journeys = await client.query(
         `SELECT id_city1, id_city2, id_transport, cost, time FROM Journeys WHERE id_transport IN (${transport_ids})`
     );
+    let totalCost = 0;
+    let totalTime = 0;
+    const path = [];
+    if (journeys.rows.length === 0) {
+        ctx.body = { path, totalCost, totalTime };
+        return;
+    }
+
     const graph = new Graph({ multigraph: true });
     journeys.rows.forEach(({ id_city1, id_city2, id_transport, cost, time }, index) =>
         graph.setEdge(id_city1, id_city2, { cost, time, id_transport }, `${id_city1}_${index}`)
     );
-
     const dijkstraResult = graphLib.alg.dijkstra(graph, depature, (e) => graph.edge(e)[sort]);
 
     let city = destination;
-    let totalCost = 0;
-    let totalTime = 0;
-    const path = [];
+    if (!dijkstraResult[city]) {
+        ctx.body = { path, totalCost, totalTime };
+        return;
+    }
+
     while (city !== depature) {
         const { distance, predecessor } = dijkstraResult[city];
         const weight = distance - dijkstraResult[predecessor].distance;
